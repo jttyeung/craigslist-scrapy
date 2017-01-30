6db5740f60503bc98ae7c3f458c72f4dec9691c0
Joanne Yeung <jttyeung@gmail.com>
2017-01-29 23:35:16 -0800

    updated ignore files

---
+++ scrapy_craigslist/scrapy_craigslist/spiders/craigslist.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# from scrapy.spider import BaseSpider
+# from scrapy.selector import HtmlXPathSelector
+# from scrapy_craigslist.items import ScrapyCraigslistItem
+
+
+class CraigslistSpider(BaseSpider):
+    name = "craigslist"
+    allowed_domains = ["craigslist.org"]
+    start_urls = ["https://sfbay.craigslist.org/search/sfc/apa"]
+
+    def parse(self, response):
+        site = HtmlXPathSelector(response)
+        # rentals = site.xpath("//li").css(".result-row")
+        rentals = site.xpath('//li[contains(@class,"result-row")]')
+
+        items = []
+
+        # for rental in rentals:
+        #     item = ScrapyCraigslistItem()
+        #     item["link"] = 'https://sfbay.craigslist.org' + rental.xpath('a[contains(@href,"/sfc")]/@href').extract()
+        #     item["price"] = rental.xpath('//span[re:test(@class, "result-price")]/text()').extract()
+        #     items.append(item)
+
+        # return items
+
+
+        for rental in rentals:
+            link = rental.xpath('a[contains(@href,"/sfc")]/@href').extract()
+            price = rental.xpath('span[re:test(@class, "result-price")]/text()').extract()
+            print link, price
+
+
+        # for listing in listing_links:
+        #     listing = listing
+        #     for price in listing:
+        #         item = ScrapyCraigslistItem
+        #         items['listing'] = listing
+        #         items['price'] = listings.xpath('//li[re:test(@class, "result-row")]//span[re:test(@class, "result-price")]/text()').extract()
+        #         return items
+
+
+        # for rental in rentals:
+        #     # item = ScrapyCraigslistItem
+        #     # links = rental.xpath("//li").css(".result-row a::attr(href)").extract()
+        #     # for link in links:
+        #     #     if link != "#":
+        #     # items['listing']
+        #     if rental.
+        #     link = rental.xpath("//li").css(".result-row a::attr(href)").extract()
+        #     # items['price']
+        #     price = rental.xpath('//li[re:test(@class, "result-row")]//span[re:test(@class, "result-price")]/text()').extract()
+        #     print link, price
